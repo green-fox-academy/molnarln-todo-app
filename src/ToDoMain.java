d562ab4d5fa6d8506507c467bfc567d49e4d1d98
Molnar Laszlo <laszlo.molnar25@gmail.com>
2019-02-08 11:59:07 +0100

    Add ToDoMain.java, todolist.txt

---
+++ src/ToDoMain.java
@@ -0,0 +1,13 @@
+public class ToDoMain {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Command Line Todo application" + "\n" + "==============================" + "\n" );
+        System.out.println(" -l List all the tasks");
+        System.out.println(" -a Add a new task");
+        System.out.println(" -r Removes a task");
+        System.out.println(" -c Completes a task");
+
+    }
+}
